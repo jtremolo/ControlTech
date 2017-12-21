a2b70d965bf7b0f5f65ca23f975432acca674968
Jordan Thistle <jtremolo06@gmail.com>
2017-12-21 14:51:58 -0600

    Added animate button

--- RamerDouglasPeuckerPlotter/src/ui/PlotWindow.C
+++ RamerDouglasPeuckerPlotter/src/ui/PlotWindow.C
@@ -7,14 +7,18 @@
 UI::PlotWindow::PlotWindow( QWidget* Parent )
   : QMainWindow( Parent ),
     m_chart( nullptr ),
-    m_lineSeries( nullptr )
+    m_lineSeries( nullptr ),
+    m_animationTimer( nullptr )
 {
   initialize();
 }
 
 
 UI::PlotWindow::~PlotWindow()
-{}
+{
+  //! Stop any running animations
+  stopAnimation();
+}
 
 
 void UI::PlotWindow::onLoadFile()
@@ -46,6 +50,9 @@ void UI::PlotWindow::onLoadFile()
 
 void UI::PlotWindow::onEpsilonChanged( double epsilon )
 {
+  //! Stop animation 
+  stopAnimation();
+
   //! Run RDP algorithm
   Data::PlotData smoothedData( m_plotData );
   smoothedData.smoothWithRDP( epsilon );
@@ -53,6 +60,38 @@ void UI::PlotWindow::onEpsilonChanged( double epsilon )
   //! Update data series
   if( m_lineSeries != nullptr )
     m_lineSeries->replace( smoothedData.data().toList() );
+}
+
+
+void UI::PlotWindow::onAnimateClicked()
+{
+  if( m_animationTimer != nullptr )
+  {
+    //! Stop previous animation
+    onEpsilonChanged( m_epsilonSpinBox->value() );
+  }
+  else
+  {
+    //! Start new animation
+    startAnimation();
+  }
+}
+
+
+void UI::PlotWindow::animateStep()
+{
+  //! Run RDP algorithm
+  Data::PlotData smoothedData( m_plotData );
+  smoothedData.smoothWithRDP( m_animationEpsilon );
+
+  //! Update data series
+  if( m_lineSeries != nullptr )
+    m_lineSeries->replace( smoothedData.data().toList() );
+
+  //! Set next epsilon value
+  m_animationEpsilon += 0.001;
+  if( m_animationEpsilon > m_epsilonSpinBox->value() )
+    m_animationEpsilon = 0.0;
 }
 
 
@@ -82,8 +121,8 @@ void UI::PlotWindow::initialize()
   mainVBox->addLayout( epsilonLayout );
 
   //! Add animate button
-  m_animateButton = new QPushButton( tr( "Animate" ) );
-  m_animateButton->setEnable( false );
+  m_animateButton = new QPushButton( tr( "Start Animation" ) );
+  m_animateButton->setEnabled( false );
 
   QHBoxLayout* animateLayout = new QHBoxLayout();
   animateLayout->addWidget( m_animateButton );
@@ -112,6 +151,8 @@ void UI::PlotWindow::initialize()
            this,           &PlotWindow::onLoadFile ); 
   connect( m_epsilonSpinBox, SIGNAL( valueChanged( double ) ),
            this,             SLOT( onEpsilonChanged( double ) ) );
+  connect( m_animateButton, &QPushButton::clicked,
+           this,            &PlotWindow::onAnimateClicked ); 
 }
 
 
@@ -162,4 +203,33 @@ bool UI::PlotWindow::readCSVFile( const QString& filePath )
   file.close();
   m_plotData = Data::PlotData( loadedPoints );
   return true;
+}
+
+
+void UI::PlotWindow::stopAnimation()
+{
+  if( m_animationTimer != nullptr )
+  {
+    m_animationTimer->stop();
+    delete m_animationTimer;
+    m_animationTimer = nullptr;
+  }
+
+  m_animateButton->setText( tr( "Start Animation" ) );
+}
+
+
+void UI::PlotWindow::startAnimation()
+{
+  //! Create the timer and reset epsilon
+  m_animationEpsilon = 0.0;
+  m_animationTimer = new QTimer( this );
+
+  //! Connect signal and start
+  connect( m_animationTimer, &QTimer::timeout,
+           this,             &PlotWindow::animateStep );
+  m_animationTimer->start( 100 );
+
+  //! Change button text
+  m_animateButton->setText( tr( "Stop Animation" ) );
 }
--- RamerDouglasPeuckerPlotter/src/ui/PlotWindow.h
+++ RamerDouglasPeuckerPlotter/src/ui/PlotWindow.h
@@ -8,21 +8,37 @@
 
 namespace UI
 {
+  //! This main window contains a plotting area
+  //! and the needed controls for the
+  //! Ramer-Douglas-Peucker algorithm.
   class PlotWindow : public QMainWindow
   {
     Q_OBJECT
 
     public:
 
+      //! Constructor
+      //!
+      //! \param Parent Parent widget to take ownership of this.
       explicit PlotWindow( QWidget* Parent = nullptr );
 
+      //! Destructor
       virtual ~PlotWindow();
 
 
     private slots:
 
+      //! Prompt user to select file and load data.
       void onLoadFile();
+
+      //! Run the RDP algortithm for new epsilon.
       void onEpsilonChanged( double epsilon );
+
+      //! Animate a range of epsilon values.
+      void onAnimateClicked();
+
+      //! Run the current animation step.
+      void animateStep();
 
 
     private:
@@ -32,6 +48,9 @@ namespace UI
       bool loadFile( const QString& filePath );
       bool readCSVFile( const QString& filePath );
 
+      void startAnimation();
+      void stopAnimation();
+
       Data::PlotData m_plotData;
 
       QChart* m_chart;
@@ -40,5 +59,8 @@ namespace UI
       QLabel* m_epsilonLabel;
       QDoubleSpinBox* m_epsilonSpinBox;
       QPushButton* m_animateButton;
+
+      double m_animationEpsilon;
+      QTimer* m_animationTimer;
   };
 }
